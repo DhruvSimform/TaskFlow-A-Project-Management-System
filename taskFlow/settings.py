8a5f5d334e4418204ab265ac6c51e2cdc9157a2b
Dhruv Patel <dhruv.n.patel@simformsolutions.com>
2025-04-02 12:31:50 +0530

    setup postgres db

--- taskFlow/settings.py
+++ taskFlow/settings.py
@@ -83,9 +83,13 @@ WSGI_APPLICATION = "taskFlow.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',  
+        'NAME': 'TaskFlow',
+        'USER': 'postgres', 
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',                        # Set to '127.0.0.1' or your server IP
+        'PORT': '5432',                             # Default PostgreSQL port
     }
 }
 
