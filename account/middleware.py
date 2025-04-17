31a144492f19bd256405243643920eb8c7495377
Dhruv Patel <dhruv.n.patel@simformsolutions.com>
2025-04-17 11:00:02 +0530

    feat: refactor AuthMiddleware to enhance request logging and streamline authentication checks

--- account/middleware.py
+++ account/middleware.py
@@ -18,9 +18,15 @@ class AuthMiddleware(MiddlewareMixin):
     Middleware to enforce user authentication for accessing URLs, except for predefined routes.
     """
 
-    def process_request(self, request):
-        # Log the user request details
+    def __call__(self, request):
+        user_ip = request.META.get("REMOTE_ADDR", "Unknown IP")
+        user_agent = request.META.get("HTTP_USER_AGENT", "Unknown User Agent")
+        logger.info(
+            f"Request from IP: {user_ip}, User Agent: {user_agent}, Path: {request.path}"
+        )
+        return super().__call__(request)
 
+    def process_request(self, request):
         """
         Handles authentication and token validation for incoming requests.
 
@@ -30,13 +36,7 @@ class AuthMiddleware(MiddlewareMixin):
         For unauthenticated users, it redirects to the login page for web/admin requests
         or allows API requests to proceed without authentication.
         """
-        user_ip = request.META.get("REMOTE_ADDR", "Unknown IP")
-        user_agent = request.META.get("HTTP_USER_AGENT", "Unknown User Agent")
-        print("")
-        logger.info(
-            f"Request from IP: {user_ip}, User Agent: {user_agent}, Path: {request.path}"
-        )
-        # Allow access to these routes without authentication
+
         allowed_routes = [
             "/api/account/login/",
             "/api/account/token/verify/",
@@ -44,34 +44,32 @@ class AuthMiddleware(MiddlewareMixin):
             "/admin/login/",
             "/api/account/request-reset-password/",
         ]
-        # return None
+
+        if (
+            request.path in allowed_routes
+            or request.path.startswith("/api/account/request-reset-password/")
+            or request.path.startswith("/admin/")
+        ):
+            return None  # Let the request pass without authentication
 
         # Extract token from the Authorization header
         auth_header = request.headers.get("Authorization", "")
         token_match = re.match(r"Bearer (.+)", auth_header)
 
-        if request.path in allowed_routes:
-            print("passed")
-            return None  # Let the request pass without authentication
-        # Allow admin panel to use session authentication
-        if request.path.startswith("/api/account/request-reset-password/"):
-            return self.get_response(request)
-        if request.path.startswith("/admin/"):
-            return None
         if not token_match:
             return JsonResponse(
                 {"error": "Your Token is not provided"},
                 status=status.HTTP_400_BAD_REQUEST,
             )
-        if token_match:
-            access_token = token_match.group(1)  # Extract token
 
-            # Check if token is blacklisted in Redis
-            if cache.get(access_token) == "blacklisted":
-                return JsonResponse(
-                    {"error": "Your Token is blacklisted"},
-                    status=status.HTTP_401_UNAUTHORIZED,
-                )
+        access_token = token_match.group(1)  # Extract token
+
+        # Check if token is blacklisted in Redis
+        if cache.get(access_token) == "blacklisted":
+            return JsonResponse(
+                {"error": "Your Token is blacklisted"},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
 
         # If user is NOT authenticated
         if not IsAuthenticated:
@@ -79,8 +77,3 @@ class AuthMiddleware(MiddlewareMixin):
                 return None
             else:  # Web/Admin request
                 return redirect(reverse("admin:login"))
-
-    def process_response(self, request, response):
-        if hasattr(response, "render") and callable(response.render):
-            response.render()  # Prevents ContentNotRenderedError
-        return response
